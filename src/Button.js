cd1564420a73f1d446ef707a02dce0b3b5f60607
shakil <shakilhossen307@gmail.com>
2024-03-16 09:05:35 +0600

    nextui

---
+++ src/Button.js
@@ -0,0 +1,8 @@
+// src/Button.js
+import React from 'react';
+
+const Button = ({ onClick, children }) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+export default Button;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'NexaUI',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
